c8248a2fdbf07c4be1061e6ebca84a1413b71514
Olivier Ross <olivier.ross@usherbrooke.ca>
2023-02-27 10:42:21 -0500

    pan limite

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 window.onload = () => {
   let basename = "cat";
+
+  // https://openseadragon.github.io/docs/
   viewer = OpenSeadragon({
     id: 'map',
     prefixUrl: `ressources/${basename}_files/`,
     tileSources: `ressources/${basename}.dzi`,
-    showNavigationControl: false // https://github.com/openseadragon/openseadragon/issues/1424
+    defaultZoomLevel: 1,
+    minZoomLevel: 1,
+    visibilityRatio: 1,
+    constrainDuringPan: true,
+    showNavigationControl: false
   });
 
   viewer.addHandler("zoom", (ev) => {
     let zoom = ev.zoom;
-    document.getElementById("nombre").innerHTML = interpretation_zoom(zoom); // https://css-tricks.com/methods-contrasting-text-backgrounds/
+    document.getElementById("nombre").innerHTML = interpretation_zoom(zoom);
   });
 }
 
 function interpretation_zoom (zoom) {
   return Math.round(zoom);
 }
-
